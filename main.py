7b57d80160bf272686ff3db7543a6029fbb93c31
P1NHE4D <aleks.michael.g2@googlemail.com>
2022-01-29 22:21:17 +0100

    add visualisations

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from nn.activation_functions import Relu, Linear
 from nn.layers import Dense, Flatten, Softmax
 from nn.loss_functions import CrossEntropy
 from datasets.shapes import Shapes
-import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
@@ -22,14 +22,14 @@ def main():
 
     s = Shapes(config)
     x_train, y_train, x_val, y_val, x_test, y_test = s.generate_dataset()
-    plt.imshow(x_train[0], cmap='gray')
-    plt.show()
 
     model.fit(x_train, y_train, x_val, y_val)
-    y_pred = model.predict(x_val[0:5])
-    for y_t, y in zip(y_val[0:5], y_pred):
-        print("GT: Rectangle: {} | Cross: {} | Circle: {} | Triangle: {}".format(y_t[0], y_t[1], y_t[2], y_t[3]))
-        print("Prediction: Rectangle: {:.2f} | Cross: {:.2f} | Circle: {:.2f} | Triangle: {:.2f}".format(y[0], y[1], y[2], y[3]))
+    y_test_pred = model.predict(x_test)
+    test_loss = []
+    for gt, pred in zip(y_test, y_test_pred):
+        loss = CrossEntropy().loss(y_pred=pred, y_true=gt)
+        test_loss.append(loss)
+    print("Test avg. loss: {:.2f}".format(np.mean(test_loss)))
 
 
 if __name__ == '__main__':
