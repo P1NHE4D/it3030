fix backprop

main.py
@@ -1,10 +1,7 @@
 import json
 
 from nn.core import SequentialNetwork
-from nn.activation_functions import Relu, Sigmoid
-import pandas as pd
-import numpy as np
-import tensorflow as tf
+from nn.activation_functions import Relu, Linear
 
 from nn.layers import Dense, Flatten, Softmax
 from nn.loss_functions import CrossEntropy
@@ -15,23 +12,12 @@ def main():
         config = json.load(f)
     model = SequentialNetwork(config)
     model.add(Flatten())
-    model.add(Dense(units=2, activation=Relu()))
-    model.add(Dense(units=10, activation=Relu()))
-    model.add(Dense(units=2, activation=Relu()))
+    model.add(Dense(units=128, activation=Relu()))
+    model.add(Dense(units=128, activation=Relu()))
+    model.add(Dense(units=10, activation=Linear()))
     model.add(Softmax())
     model.compile(loss_function=CrossEntropy())
 
-    x_train = pd.read_csv("data/IRISdata.csv", usecols=["SepalWidthCm", "PetalWidthCm"]).to_numpy()
-    y_train = pd.read_csv("data/IRIStargets.csv", usecols=["Species"])
-    y_train[y_train.Species == -1] = 0
-    y_train = y_train.to_numpy()
-
-    # normalise data
-    #x_train = tf.keras.utils.normalize(x_train, axis=1)
-    y_train = tf.one_hot(y_train, 2)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_train)
-    print(y_train, y_pred)
 
 if __name__ == '__main__':
     main()

nn/layers.py
@@ -1,7 +1,5 @@
 from abc import abstractmethod, ABC
-
 from numpy import ndarray
-
 from nn.activation_functions import ActivationFunction
 import numpy as np
 
@@ -32,13 +30,10 @@ class Dense(Layer):
         self.activation = activation
         self.weights = None
         self.input = None
-        self.output_sum = None
         self.weight_update = None
+        self.output = None
 
     def forward(self, X: ndarray):
-        """
-        :return: activation value based on the input
-        """
         # add 1 for bias
         X = np.hstack([X, [1]])
 
@@ -49,13 +44,14 @@ class Dense(Layer):
 
         # store data for backpropagation
         self.input = X
-        self.output_sum = np.dot(X, self.weights)
+        output_sum = np.dot(X, self.weights)
+        self.output = self.activation.function(output_sum)
 
         # compute activation value
-        return self.activation.function(self.output_sum)
+        return self.output
 
     def backward(self, J_L_N: ndarray):
-        J_N_SUM = np.diag(self.activation.gradient(self.output_sum))
+        J_N_SUM = np.diag(self.activation.gradient(self.output))
         J_N_M = np.dot(J_N_SUM, self.weights[:-1].T)
         J_N_W = np.outer(self.input, J_N_SUM.diagonal())
         J_L_M = np.dot(J_L_N, J_N_M)
@@ -72,14 +68,14 @@ class Dense(Layer):
 class Softmax(Layer):
 
     def __init__(self):
-        self.input = None
+        self.output = None
 
     def forward(self, X):
-        self.input = X
-        return np.exp(X) / np.exp(X).sum()
+        self.output = np.exp(X) / np.exp(X).sum()
+        return self.output
 
     def backward(self, J_L_Z: ndarray):
-        J_S = np.diag(self.input) - np.outer(self.input, self.input)
+        J_S = np.diag(self.output) - np.outer(self.output, self.output)
         return np.dot(J_L_Z, J_S)
 
     def update_weights(self, learning_rate):

nn/loss_functions.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-
 import numpy as np
 
 
@@ -26,7 +25,7 @@ class MSE(LossFunction):
 
 class CrossEntropy(LossFunction):
     def loss(self, y_pred, y_true):
-        return -np.sum(y_true * np.log(y_pred))
+        return -np.sum(y_true * np.log2(y_pred))
 
     def gradient(self, y_pred, y_true):
         return y_pred - y_true